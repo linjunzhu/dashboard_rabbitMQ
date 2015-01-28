a35c0ea3f4508f3d081cc7e4bb2956dd05c5393c
linjunzhu <linjunzhugg@gmail.com>
2015-01-28 14:12:19 +0800

    finish

---
+++ app/worker/posts_worker.rb
@@ -0,0 +1,18 @@
+class PostsWorker
+  include Sneakers::Worker
+  # This worker will connect to "dashboard.posts" queue
+  # env is set to nil since by default the actuall queue name would be
+  # "dashboard.posts_development"
+
+  # 这里同时也会创建队列
+  from_queue "dashboard.posts", env: nil
+
+  # work method receives message payload in raw format
+  # in our case it is JSON encoded string
+  # which we can pass to RecentPosts service without
+  # changes
+  def work(raw_post)
+    RecentPosts.push(raw_post)
+    ack! # we need to let queue know that message was received
+  end
+end
